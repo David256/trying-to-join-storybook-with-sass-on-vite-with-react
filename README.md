8977b2759e821ff120ccb32d36ef0ff58c2c4b8e
David256 <wasterdavid@gmail.com>
2022-05-18 22:00:29 -0500

    Prepare initial code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Trying to Join the Storybook with SASS on vite.js with React
+
+I did:
+
+```bash
+npm create vite .
+```
+
+And selected the template `react-ts`.
+
+Modified the files in `src/` and wrote a test page.
+
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Trying to Join the Storybook with SASS on vite.js with React</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
