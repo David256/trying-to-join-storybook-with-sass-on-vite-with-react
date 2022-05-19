e119dceba29b167a5aacdbe8642ad75d2d4ca3df
David256 <wasterdavid@gmail.com>
2022-05-18 22:39:25 -0500

    Put the button in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+import { SpatialButton } from './components/SpatialButton/SpacialButton';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Trying to Join the Storybook with SASS on vite.js with React</p>
+
+        <SpatialButton value="Click me" />
       </header>
     </div>
   )
